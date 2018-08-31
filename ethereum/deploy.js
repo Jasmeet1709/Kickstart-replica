4e353f8e809dd8670123ac6e2479bd058d5ad9a3
Jasmeet1709 <jsmtbagga45@gmail.com>
2018-09-01 00:10:34 +0530

    deployed the contract on rinkeby test network

---
+++ ethereum/deploy.js
@@ -0,0 +1,21 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+    'raise woman envelope arrest fall one coconut slogan public uncle lesson hedgehog',
+    'https://rinkeby.infura.io/v3/0e697c2d10f9428da44fbbfd613fb18f'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async()=>{
+    const accounts = await web3.eth.getAccounts();
+    console.log('attempting to deploy from account', accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: '0x'+ compiledFactory.bytecode ,arguments: ['Hello']})
+        .send({from: accounts[0], gas:'1000000'});
+
+    console.log('Contract deployed to ',result.options.address);
+};
+deploy();
